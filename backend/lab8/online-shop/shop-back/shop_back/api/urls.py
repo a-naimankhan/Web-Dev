61f58243bf083c3e8b131847e995b1da6d1160da
a-naimankhan <naimakhanaibar@gmai.com>
2026-04-08 01:56:12 +0500

    feat : added fbv product list and product detail

--- backend/lab8/online-shop/shop-back/shop_back/api/urls.py
+++ backend/lab8/online-shop/shop-back/shop_back/api/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CategoryViewSet
+from .fbv import product_list , product_detail
 
-router = DefaultRouter()
-router.register('categories', CategoryViewSet)
-router.register('products', ProductViewSet)
+#
+# router = DefaultRouter()
+# router.register('categories', CategoryViewSet)
+# router.register('products', ProductViewSet)
+#
+# urlpatterns = router.urls
 
-urlpatterns = router.urls
+urlpatters = [
+    path('products/', product_list, name='products-list'),
+    path('products/<int:pk>/', product_detail, name='product-detail'),
+]
